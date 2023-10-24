13a87cd81141ba54342e2818916a31cf06917f34
JDRL555 <joshuleal3@gmail.com>
2023-10-24 11:12:49 -0400

    style:added the favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React    from 'react'
 import ReactDOM       from 'react-dom/client'
 import GlobalProvider from './utils/redux/GlobalProvider'
 import App            from './App.jsx'
 import './static/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
-  </React.StrictMode>,
+  <GlobalProvider>
+    <App />
+  </GlobalProvider>
 )
