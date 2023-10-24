030a8d715896ec71e152acc0de1e19ba450ab50d
JDRL555 <joshuleal3@gmail.com>
2023-10-24 12:23:41 -0400

    fix:modified the fetch to async

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,4 +1,4 @@
-import { useState }                             from 'react'
+import { useState, useEffect }                  from 'react'
 import { useNavigate }                          from 'react-router-dom'
 import { useDispatch }                          from 'react-redux'
 import { useCookies }                           from 'react-cookie'
@@ -14,17 +14,20 @@ export default function Protected({children}) {
 
   const [loading, setLoading] = useState(true)
 
-  useFetch(`${SERVER_URL}${AUTH_ROUTE}?access_token=${token.access_token}`)
-  .then(user_response => {
-    if(!user_response?.data) navigate("/")
-    else {
-      useFetch(`${SERVER_URL}${TASKS_ROUTE}?user_id=${user_response.data._id}`)
-      .then(task_response => {
+  useEffect(() => {
+    async function getData() {
+      const user_response = await useFetch(`${SERVER_URL}${AUTH_ROUTE}?access_token=${token.access_token}`)
+      console.log(user_response)
+      if(!user_response?.data) navigate("/")
+      else {
+        const task_response = await useFetch(`${SERVER_URL}${TASKS_ROUTE}?user_id=${user_response.data.id}`)
         dispatch(fetchTasks({tasks: task_response.data.tasks, user: user_response.data}))
         setLoading(false)
-      })
+      }
     }
+    getData()
   })
+
 
   if(!loading) {
     return (
